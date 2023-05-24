be643606a39b7919d552c73f1ba633360d7f87ae
youngcs97 <chris@chrisyoung.us>
2023-05-24 19:50:57 -0400

    Javascript for inline editing of Curl commands

--- public/js/curl-tokens.js
+++ public/js/curl-tokens.js
@@ -7,4 +7,27 @@ window.addEventListener('DOMContentLoaded', event => {
             document.getElementById("curl-http").innerText=text
         }, false)
     })
+    
+    Array.from(document.querySelectorAll('#token')).forEach(elem => {
+        elem.addEventListener('input', e => { 
+            const r = /"token=[^"]*/i
+            const c = document.getElementById("curl-http")
+            c.innerText = c.innerText.replace(r,"\"token="+elem.value)
+        }, false)
+    })
+    Array.from(document.querySelectorAll('#file')).forEach(elem => {
+        elem.addEventListener('input', e => { 
+            const r = /"file=@[^"]*/i
+            const c = document.getElementById("curl-http")
+            const v = elem.value.split("\\")
+            c.innerText = c.innerText.replace(r,"\"file=@"+v[v.length-1])
+        }, false)
+    })
+    Array.from(document.querySelectorAll('#authorization')).forEach(elem => {
+        elem.addEventListener('input', e => { 
+            const r = /"authorization=[^"]*/i
+            const c = document.getElementById("curl-http")
+            c.innerText = c.innerText.replace(r,"\"authorization="+elem.value)
+        }, false)
+    })
 })
