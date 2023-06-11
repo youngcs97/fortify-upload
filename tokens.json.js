f5ac3981a284a46ed5fb3fa68f0e539d7fa85452
youngcs97 <chris@chrisyoung.us>
2023-06-11 14:09:07 -0400

    Update tokens.json.js
    Updated to support same method signatures as cosmos

--- tokens.json.js
+++ tokens.json.js
@@ -1,12 +1,23 @@
 const fs = require("fs")
 const path = "./.tokens.json"
 const config = require("./.config.json")
+const utils = require("./util.js")
 
 const tokens = {
-    get: async function(id) {
+    init: async function(loops=10) {
+        const c = config.tokens
+        let t = []
+        for (let i=0; i<loops; i++) {
+            for (let j=1; j<4; j++) {
+                t.push({ id: utils.token(), token: config.tokens.default, project: "Testing", version: j+".0", user: `testing-${j}.0@${c.container}.com`})
+            }
+        }
+        require("fs").writeFileSync("./.tokens.json", JSON.stringify({ tokens: t },null,4))
+    },
+    get: async function(id, sast=false) {
         return new Promise(async function(resolve, reject) {
             fs.readFile(path,"utf-8", (err, data)=> {
-                r = JSON.parse(data).tokens.find(x => x.id==id)  // find token by key:name
+                r = JSON.parse(data).tokens.find(x => x.id==id)  // find token by id:name
                 if (r==null) resolve(null)
                 r.url = config.sast.url
                 r.client = config.sast.client
@@ -28,6 +39,72 @@ const tokens = {
             })
         })
     },
+    reenable: async function(id) {
+        return this.submission(id, { message: "re-enabled" })
+    },
+    report: async function(id) {
+        return new Promise(async function(resolve, reject) {
+            fs.readFile(path,"utf-8", (err, data)=> {
+                const r = JSON.parse(data)
+                const t = r.tokens.find(x => x.id==id)
+                t.report = new Date(Date.now()).toISOString()
+                fs.writeFile(path,JSON.stringify(r, null, 4),"utf-8",()=> {
+                    resolve(t)
+                })
+            })
+        })
+    },
+    delete: async function(id) {
+        return new Promise(async function(resolve, reject) {
+            fs.readFile(path,"utf-8", (err, data)=> {
+                const r = JSON.parse(data)
+                r.tokens = r.tokens.filter(x => { return x.id!=id })
+                fs.writeFile(path,JSON.stringify(r, null, 4),"utf-8",()=> {
+                    resolve({"id": id})
+                })
+            })
+        })
+    },
+    authorized: function(authorization) {
+        return (config.tokens.authorization==authorization)
+    },
+    getAll: async function(query, page=0) {
+        return new Promise(async function(resolve, reject) {
+            const x = { url: config.sast.url, client: config.sast.client }
+            fs.readFile(path,"utf-8", (err, data)=> {
+                let s = JSON.parse(data).tokens
+                const v = ["token","project","version","user"]
+                for (let r in query) {
+                    let t = r.trim().toLowerCase()
+                    if (v.includes(t)) {
+                        s = s.filter(x => { return x[t]==query[r] })
+                    }
+                }
+                resolve(s)
+            })
+        })
+    },
+
+    // below this need to be remediated to json
+    update: async function(id, update) {
+        return new Promise(async function(resolve, reject) {
+            fs.readFile(path,"utf-8", (err, data)=> {
+                const r = JSON.parse(data)
+                const t = r.tokens.find(x => x.id==id)
+                const v = ["token","project","version","user"]
+                for (let u in update) {
+                    let p = u.trim().toLowerCase()
+                    if (v.includes(p)) {
+                        t[p]=update[u]
+                    }
+                }
+                fs.writeFile(path,JSON.stringify(r, null, 4),"utf-8",()=> {
+                    resolve(t)
+                })
+            })
+        })
+    }
+
 }
 module.exports = tokens
 
